e3e34678140f4caafb9961d38eaba4fac79b3545
irrationnelle <drakkarverenis@gmail.com>
2020-04-07 23:21:42 +0900

    add test for each concert component

--- src/concert-list.test.tsx
+++ src/concert-list.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import ConcertList from './concert-list';
 
+
 test('renders concert list component', () => {
   const { getByText } = render(<ConcertList />);
   const linkElement = getByText(/concert list/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders each concert components', () => {
+  const { getByText } = render(<ConcertList />);
+  const linkElement = getByText(/megadeth/);
   expect(linkElement).toBeInTheDocument();
 });
