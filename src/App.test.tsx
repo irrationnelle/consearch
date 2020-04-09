e0babbfab0c520f6d535d776f26023ffcba43670
irrationnelle <drakkarverenis@gmail.com>
2020-04-09 23:33:33 +0900

    Add new test for assertion that verifies importing ConcertList component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders each concert components', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/megadeth/);
+  expect(linkElement).toBeInTheDocument();
+});
