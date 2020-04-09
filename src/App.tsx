158c34203e4962da553ec1052b155f8ceadd3121
irrationnelle <drakkarverenis@gmail.com>
2020-04-09 23:33:44 +0900

    Add ConcertList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import ConcertList from "./concert-list";
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ConcertList/>
     </div>
   );
 }
