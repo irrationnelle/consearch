4574dcf7883f8443f6c7cc797c1ae5e63ab04253
irrationnelle <drakkarverenis@gmail.com>
2020-04-11 23:51:19 +0900

    implement menu component

---
+++ src/menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Menu() {
+   const list = 'list';
+
+  return (
+      <div>
+          <div>
+            <span>{list}</span>
+          </div>
+      </div>
+  );
+}
+
+export default Menu;
