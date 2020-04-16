5f72b3cc748e8d8c7d5f4183532549d85df6b374
Hyeonseok AHN <drakkarverenis@gmail.com>
2020-04-16 23:51:28 +0900

    Add kakao map api to App.tsx Delete kakao map api from index.html and concert.tsx Implement env with .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import ConcertList from "./concert-list";
 import Concert from "./concert";
@@ -11,6 +11,28 @@ declare global {
 }
 
 function App() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.type = 'text/javascript';
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_API_KEY}&autoload=false`;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            const { kakao } = window;
+            kakao.maps.load(() => {
+                const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+                const options = {
+                    //지도를 생성할 때 필요한 기본 옵션
+                    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+                    level: 3 //지도의 레벨(확대, 축소 정도)
+                };
+
+                // const map = new kakao.maps.Map(container, options);
+            });
+        };
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
