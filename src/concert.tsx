30dd372645f6d09d8ea8ab027a075891c4a13099
irrationnelle <drakkarverenis@gmail.com>
2020-04-19 23:52:50 +0900

    Simplify concert.tsx with ConcertLocationMap component and points props

--- src/concert.tsx
+++ src/concert.tsx
@@ -1,19 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ConcertLocationMap from "./ConcertLocationMap";
 
 function Concert() {
   const title = 'cult of luna';
   const price = 10000;
+  const [points, setPoints] = useState({x: 33.450701, y: 126.570667});
 
   useEffect(() => {
-      const { kakao } = window;
-      const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-      const options = { //지도를 생성할 때 필요한 기본 옵션
-          center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-          level: 3 //지도의 레벨(확대, 축소 정도)
-      };
-
-      const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-  }, [])
+      setPoints({x: 37.54839846253257, y: 126.92007036208365});
+      }, [])
 
   return (
     <div>
@@ -25,7 +20,7 @@ function Concert() {
           <span>price</span>
           <span>{price}</span>
         </div>
-        <div id="map" style={{width: '500px', height:'400px'}}></div>
+        <ConcertLocationMap points={points} />
     </div>
   );
 }
