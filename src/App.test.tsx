7668a4351d86b334e0f232bd208a92fb5f19f963
irrationnelle <drakkarverenis@gmail.com>
2020-04-30 23:05:02 +0900

    Delete useless test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('renders each concert components', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/amenra/);
