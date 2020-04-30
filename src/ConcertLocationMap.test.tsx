e32e720fd246b699c53141143691e86138daf972
irrationnelle <drakkarverenis@gmail.com>
2020-04-30 23:05:58 +0900

    Work in progress to fix rendering test for global window module by third-party

--- src/ConcertLocationMap.test.tsx
+++ src/ConcertLocationMap.test.tsx
@@ -3,7 +3,29 @@ import { render } from '@testing-library/react';
 import ConcertLocationMap from './ConcertLocationMap';
 
 test('renders concert list component', () => {
-    const component  = render(<ConcertLocationMap/>);
+
+    global.kakao = jest.fn().mockImplementation(()=>{
+        return {maps: {
+            Map: ()=>{return null;},
+            LatLng: ()=>{return null;},
+            services: {}
+        }}
+    });
+
+    let mockLatLng = jest.fn();
+    global.kakao.maps = jest.fn().mockImplementation(()=> {
+        return {
+            LatLng: mockLatLng
+        }
+        /*
+        return {
+            LatLng: jest.fn().mockResolvedValue(null)
+        }
+         */
+    });
+
+
+    const component  = render(<ConcertLocationMap points={{x: 1,  y: 2}}/>);
     expect(component).toBeInTheDocument();
 });
 
