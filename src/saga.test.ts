3813662558261fe9458502be9d9cc85d64271f07
irrationnelle <drakkarverenis@gmail.com>
2020-05-11 23:54:09 +0900

    Modifying redux saga test

--- src/saga.test.ts
+++ src/saga.test.ts
@@ -2,22 +2,11 @@ import { fork, take, takeLatest } from 'redux-saga/effects'
 
 import { mySaga, fetchConcerts } from './sagas'
 
-const reqConcerts = () => ({
-  type: "REQ_CONCERTS_2"
-});
-
-
-function* fetchSaga() {
-  const action = yield take(reqConcerts);
-}
-
-
 test('fetch concerts', done => {
   const gen = mySaga();
     const expected = fork(takeLatest, 'REQ_CONCERTS', fetchConcerts)
 
-    expect(gen.next().value).toMatchObject(takeLatest('REQ_CONCERTS', fetchConcerts));
-
+    expect(gen.next().value).toEqual(takeLatest('REQ_CONCERTS', fetchConcerts));
 
     expect(gen.next().done).toEqual(true);
 
