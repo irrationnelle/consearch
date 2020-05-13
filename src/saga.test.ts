3339dc2897d6c1c0a782af1446858fed2f219af4
Hyeonseok AHN <drakkarverenis@gmail.com>
2020-05-13 23:55:13 +0900

    Delete mocking api to test actual saga

--- src/saga.test.ts
+++ src/saga.test.ts
@@ -6,9 +6,10 @@ import { mySaga, api } from './sagas'
 it("fetches concerts", () => {
   const id = 42;
   const user = { id, name: "Jeremy" };
+  const user2 = { id, name: "Henry" };
 
   return expectSaga(mySaga)
-    .provide([[call(api, id), user]])
+    //.provide([[call(api, id), user]])
     .put({ type: "REQ_CONCERTS_SUCCEEDED", payload: user })
     .dispatch({ type: "REQ_CONCERTS", payload: id })
     .silentRun();
