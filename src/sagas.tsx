2a393bd7479a0a83c097c0a36a62b3c9dfcd8bed
irrationnelle <drakkarverenis@gmail.com>
2020-05-29 23:57:18 +0900

    fix: modify action.payload for reducer

--- src/sagas.tsx
+++ src/sagas.tsx
@@ -1,6 +1,6 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 
-const api = (id: number) => { return { id, name: "Jeremy" }}
+const api = (id: number) => { return [{ id: 1, title: 'behemoth', price: 20000, address: 'norway' }, {id: 2, title: 'shining', price: 10000, address: "poland"}]}
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchConcerts(action: any) {
