34d84a4bf35a45c8254d3722a62ddcbb788781dd
irrationnelle <drakkarverenis@gmail.com>
2020-05-30 16:53:54 +0900

    fix: add cleanup function for setTimeout

--- src/concert.tsx
+++ src/concert.tsx
@@ -4,10 +4,17 @@ import { History, Location } from "history";
 
 import ConcertLocationMap from "./ConcertLocationMap";
 
+interface Concert {
+    id: string;
+    title: string;
+    artist: string;
+    address: string;
+    price: number;
+}
+
 function Concert(props: {key: string; name?: string;}) {
     const history: History = useHistory();
-    const {state: { title }}: Location<{title: string}> = useLocation();
-    const price = 10000;
+    const {state: { title, price }}: Location<{title: string, price: number;}> = useLocation();
 
   const [points, setPoints] = useState({x: 33.450701, y: 126.570667});
   const [address, setAddress] = useState('제주특별자치도 제주시 첨단로 242');
@@ -19,9 +26,13 @@ function Concert(props: {key: string; name?: string;}) {
   useEffect(() => {
       setPoints({x: 37.54839846253257, y: 126.92007036208365});
 
-      setTimeout(() => {
-          setAddress('서울특별시 마포구 서교동 홍익로 25');
+      const timeout = setTimeout(() => {
+        setAddress('서울특별시 마포구 서교동 홍익로 25');
       }, 1500);
+
+      return () => {
+          clearTimeout(timeout);
+      }
   }, [])
 
   return (
