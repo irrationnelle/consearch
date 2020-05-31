86d07cbf69b233ba997d3ef12e0287494819e9a8
irrationnelle <drakkarverenis@gmail.com>
2020-05-31 17:28:42 +0900

    chore: devide type info and mock data using globally

---
+++ src/@models/concert.ts
@@ -0,0 +1,7 @@
+export interface Concert {
+    id: number;
+    title: string;
+    artist: string;
+    address: string;
+    price: number;
+}
---
+++ src/__mock__/data.ts
@@ -0,0 +1,20 @@
+import { Concert } from "../@models/concert";
+
+const mockConcerts: Concert[] = [
+    {
+        id: 1,
+        title: "behemoth",
+        artist: "behemoth",
+        price: 20000,
+        address: "norway"
+    },
+    {
+        id: 2,
+        title: "shining",
+        artist: "shining",
+        price: 10000,
+        address: "poland"
+    }
+];
+
+export { mockConcerts };
