0c5c818ba9fd880a5a46f070de5d9ab4622974da
irrationnelle <drakkarverenis@gmail.com>
2020-06-08 20:58:07 +0900

    test: add properties to mock data to pass test

--- src/__mock__/data.ts
+++ src/__mock__/data.ts
@@ -3,15 +3,23 @@ import { Concert } from "../@models/concert";
 const mockConcerts: Concert[] = [
     {
         id: 1,
+        artist: {
+            name: "behemoth",
+            genre: "blackmetal"
+        },
+        timetable: "2020-06-10",
         title: "behemoth",
-        artist: "behemoth",
         price: 20000,
         address: "norway"
     },
     {
         id: 2,
+        artist: {
+            name: "shining",
+            genre: "blackmetal"
+        },
+        timetable: "2020-09-23",
         title: "shining",
-        artist: "shining",
         price: 10000,
         address: "poland"
     }
