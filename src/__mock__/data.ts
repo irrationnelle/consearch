76ac0adb3cf5b9b8e7a0fac395e1b6aec1f9bf08
irrationnelle <drakkarverenis@gmail.com>
2020-06-11 22:39:30 +0900

    chore: modify by prettier

--- src/__mock__/data.ts
+++ src/__mock__/data.ts
@@ -1,5 +1,5 @@
 import { Concert } from "../@models/concert";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 const mockConcerts: Concert[] = [
     {
@@ -8,7 +8,7 @@ const mockConcerts: Concert[] = [
             name: "behemoth",
             genre: "blackmetal"
         },
-        timetable: format(new Date(2020, 5, 24), 'yyyy-MM-dd'),
+        timetable: format(new Date(2020, 5, 24), "yyyy-MM-dd"),
         title: "behemoth",
         price: 20000,
         address: "norway"
@@ -19,7 +19,7 @@ const mockConcerts: Concert[] = [
             name: "shining",
             genre: "blackmetal"
         },
-        timetable: format(new Date(2020, 9, 24), 'yyyy-MM-dd'),
+        timetable: format(new Date(2020, 9, 24), "yyyy-MM-dd"),
         title: "shining",
         price: 10000,
         address: "poland"
