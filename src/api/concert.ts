f88740224336484f8f0b0190e4dd9850a9ad2d05
irrationnelle <drakkarverenis@gmail.com>
2020-06-11 22:40:25 +0900

    feat: add single retrieve api with fake url and replace domain url with env variable

--- src/api/concert.ts
+++ src/api/concert.ts
@@ -2,13 +2,26 @@ import axios from "axios";
 import { Concert } from "../@models/concert";
 import { mockConcerts } from "../__mock__/data";
 
+const { REACT_APP_DOMAIN_API_URL } = process.env;
+
 const retrieveConcerts = async (): Promise<Concert[]> => {
     if (process.env.REACT_APP_DEV) {
         const result = await Promise.resolve(mockConcerts);
         return result;
     }
-    const { data } = await axios.get("fakeUrl.dev/concerts");
+    const { data } = await axios.get(`${REACT_APP_DOMAIN_API_URL}/concerts`);
     return data;
 };
 
-export { retrieveConcerts };
+const retrieveConcert = async (id: number): Promise<Concert> => {
+    if (process.env.REACT_APP_DEV) {
+        const result = await Promise.resolve(mockConcerts[0]);
+        return result;
+    }
+    const { data } = await axios.get(
+        `${REACT_APP_DOMAIN_API_URL}/concerts/${id}`
+    );
+    return data;
+};
+
+export { retrieveConcerts, retrieveConcert };
