3d83f057c5b98758ff7b85e33d40289f0ae31a23
irrationnelle <drakkarverenis@gmail.com>
2020-06-14 03:14:19 +0900

    test: modify api url with env

--- src/server-handlers.ts
+++ src/server-handlers.ts
@@ -1,9 +1,11 @@
 import { setupWorker, rest } from "msw";
 import { mockConcerts } from "./__mock__/data";
 
+const { REACT_APP_DOMAIN_API_URL } = process.env;
+
 // 2. Define request handlers and response resolvers
 const worker = setupWorker(
-    rest.get("https://fake.api.dev/concerts", (req, res, ctx) => {
+    rest.get(`${REACT_APP_DOMAIN_API_URL}/concerts`, (req, res, ctx) => {
         return res(
             ctx.delay(1500),
             ctx.status(202, "Mocked status"),
