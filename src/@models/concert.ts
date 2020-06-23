c86d941b2560b8bb3ccf5c453cabe9b6bbf9c1e8
irrationnelle <drakkarverenis@gmail.com>
2020-06-24 02:35:32 +0900

    chore: remove repetition on type definition for concert

--- src/@models/concert.ts
+++ src/@models/concert.ts
@@ -1,4 +1,4 @@
-export interface RawConcert {
+interface ConcertWithoutTime {
     id: number;
     artist: {
         name: string;
@@ -7,18 +7,13 @@ export interface RawConcert {
     title: string;
     address: string;
     price: number;
+}
+
+export interface RawConcert extends ConcertWithoutTime{
     timetable: string;
 }
 
-export interface Concert {
-    id: number;
-    artist: {
-        name: string;
-        genre: string;
-    }[];
-    title: string;
-    address: string;
-    price: number;
+export interface Concert extends ConcertWithoutTime{
     time: string;
     date: string;
 }
