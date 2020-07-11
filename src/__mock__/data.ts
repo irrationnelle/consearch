681283b814fd1a8bbf1178acbcb9394dd72bed03
irrationnelle <drakkarverenis@gmail.com>
2020-07-11 23:28:11 +0900

    test: add test to assert filtered concerts by genre

--- src/__mock__/data.ts
+++ src/__mock__/data.ts
@@ -22,6 +22,28 @@ const mockConcerts: RawConcert[] = [
         title: "Shining",
         price: 15000,
         address: "Stanisława Noakowskiego 16, 00-666 Warszawa, Poland"
+    },
+    {
+        id: 3,
+        artists: [{
+            name: "Lamb of god",
+            genre: "Metalcore"
+        }],
+        timetable: "2020-07-10T20:00:00+09:00",
+        title: "Lamb of god",
+        price: 30000,
+        address: "Conne Island, Koburger Str. 3, 04277 Leipzig"
+    },
+    {
+        id: 4,
+        artists: [{
+            name: "Killswitch engage",
+            genre: "Metalcore"
+        }],
+        timetable: "2020-07-17T20:00:00+09:00",
+        title: "Killswitch engage",
+        price: 20000,
+        address: "Kensington Gore, South Kensington, London"
     }
 ];
 
