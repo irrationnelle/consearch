4438d8fe4a8217aa1b97d3e05f1ab06cf6601d08
irrationnelle <drakkarverenis@gmail.com>
2020-09-04 23:46:39 +0900

    feature: add genre action to control search concert with genre

--- src/reducers.tsx
+++ src/reducers.tsx
@@ -2,19 +2,44 @@ import { RawConcert } from './@models/concert';
 
 const initialState: {
     concerts: RawConcert[];
+    inputGenres: string[]
 } = {
   concerts: [],
+  inputGenres: [],
 };
+
+interface Action {
+  type: string;
+}
+
+interface GenreAction extends Action{
+    payload: {
+      genres: string[]
+    }
+}
+
+interface ConcertsAction extends Action {
+    payload: {
+      concerts: RawConcert[]
+    }
+}
+
+type TotalAction = ConcertsAction | GenreAction;
 
 function concert(
   state = initialState,
-  action: { type: string; payload: { concerts: RawConcert[] } },
-) : {concerts: RawConcert[]} {
+  action: TotalAction,
+) : { concerts: RawConcert[]; inputGenres: string[] } {
   switch (action.type) {
     case 'concerts/readSuccess':
       return {
         ...state,
-        concerts: [...action.payload.concerts],
+        concerts: [...(action as ConcertsAction).payload.concerts],
+      };
+    case 'concerts/addGenre':
+      return {
+        ...state,
+        inputGenres: [...(action as GenreAction).payload.genres],
       };
     case 'concerts/readFail':
     default:
