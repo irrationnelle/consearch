37a5a671b1b5555aa27e658101d98cf8406455db
irrationnelle <drakkarverenis@gmail.com>
2020-12-03 01:15:49 +0900

    feat: align App component to center

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import React, { ReactElement } from 'react';
+import React, { FC, ReactElement } from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import styled from 'styled-components';
 
 import ConcertList from './ConcertList';
-import './App.css';
 
-const App: React.FC = (): ReactElement => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Main</Link>
-        </li>
-        <li>
-          <Link to="/concerts">Concerts</Link>
-        </li>
-      </ul>
+const App: FC = (): ReactElement => (
+  <Wrapper>
+    <Router>
+      <AppWrapper>
+        <ul>
+          <li>
+            <Link to="/">Main</Link>
+          </li>
+          <li>
+            <Link to="/concerts">Concerts</Link>
+          </li>
+        </ul>
 
-      <Switch>
-        <Route path="/concerts">
-          <ConcertList />
-        </Route>
-        <Route path="/">
-          <div>main</div>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+        <Switch>
+          <Route path="/concerts">
+            <ConcertList />
+          </Route>
+          <Route path="/">
+            <div>main</div>
+          </Route>
+        </Switch>
+      </AppWrapper>
+    </Router>
+  </Wrapper>
 );
+
+const Wrapper = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+const AppWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+    border: 1px solid black;
+    padding-right: 8px;
+    padding-left: 8px;
+`;
 
 export default App;
