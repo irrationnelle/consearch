d34131e406586dd672af1d76d6d40782becff31a
irrationnelle <drakkarverenis@gmail.com>
2021-07-15 17:16:46 +0900

    fix: Import firebase app from 'firebase/app' instead of 'firebase/compact'

--- src/hooks/useFirebase.tsx
+++ src/hooks/useFirebase.tsx
@@ -1,17 +1,15 @@
 import { useEffect } from 'react';
-import firebase from 'firebase/compat';
+import * as firebase from 'firebase/app';
 
 const useFirebase = (): void => {
   useEffect(() => {
-    if (!firebase.apps.length) {
+    if (firebase.getApps().length === 0) {
       firebase.initializeApp({
         apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
         authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
         storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
       });
-    } else {
-      firebase.app(); // if already initialized, use that one
     }
   }, []);
 };
