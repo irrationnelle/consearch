98ddbb0da8c28e2efc3ed884c10967cb438d23f3
irrationnelle <drakkarverenis@gmail.com>
2021-07-20 19:17:18 +0900

    feat: Add test to check concert list and Implement concerts list with static values

--- src/ReadConcerts.tsx
+++ src/ReadConcerts.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const ReadConcerts = () => (
-  <div data-testid="read-concerts" />
-);
+const ReadConcerts = () => {
+  useEffect(() => {
+    console.log('hello');
+  }, []);
+
+  return (
+    <div data-testid="read-concerts">
+      <div role="list">
+        <div role="listitem">
+          <span>마스토돈</span>
+          <span>마스토돈</span>
+          <span>rockmetal</span>
+          <span>롤링홀</span>
+          <span>서울 마포구 어울마당로 35</span>
+          <span>2021-06-26-20:00</span>
+        </div>
+        <div role="listitem">
+          <span>라니아</span>
+          <span>라니아 공연</span>
+          <span>인디락</span>
+          <span>FF</span>
+          <span>서울 마포구 어울마당로 37</span>
+          <span>2021-07-20-20:00</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ReadConcerts;
--- src/__test__/ReadConcerts.test.tsx
+++ src/__test__/ReadConcerts.test.tsx
@@ -13,4 +13,44 @@ describe('ReadConcerts 에서는,', () => {
     const span = screen.getByTestId('read-concerts');
     expect(span).toBeInTheDocument();
   });
+
+  it('컴포넌트를 렌더링하면 공연 목록을 읽는다.', () => {
+    const concerts = screen.getAllByRole('listitem');
+    const firstConcert = concerts[0];
+
+    const EXAMPLE_DATA = {
+      title: '마스토돈',
+      artist: '마스토돈',
+      stage: '롤링홀',
+      address: '서울 마포구 어울마당로 35',
+      genre: 'rockmetal',
+      date: '2021-06-26-20:00',
+      coverImage: '',
+    };
+
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.title);
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.artist);
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.genre);
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.stage);
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.address);
+    expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.date);
+
+    const secondConcert = concerts[1];
+    const SECOND_EXAMPLE_DATA = {
+      title: '라니아 공연',
+      artist: '라니아',
+      stage: 'FF',
+      address: '서울 마포구 어울마당로 37',
+      genre: '인디락',
+      date: '2021-07-20-20:00',
+      coverImage: '',
+    };
+
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.title);
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.artist);
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.genre);
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.stage);
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.address);
+    expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.date);
+  });
 });
