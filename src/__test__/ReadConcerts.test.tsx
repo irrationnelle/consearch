b7c416250667450912776463e0ec380cdcaaceed
irrationnelle <drakkarverenis@gmail.com>
2021-07-22 15:00:58 +0900

    test: Add test to search certain concert info

--- src/__test__/ReadConcerts.test.tsx
+++ src/__test__/ReadConcerts.test.tsx
@@ -3,6 +3,27 @@ import { waitFor } from '@testing-library/react';
 import { renderWithProviders, screen } from '../helpers/test-utils';
 import * as api from '../api/concert';
 import ReadConcerts from '../ReadConcerts';
+import inputTextByLabel from './helper/inputTextByLabel';
+
+const EXAMPLE_DATA = {
+  title: '마스토돈',
+  artist: '마스토돈',
+  stage: '롤링홀',
+  address: '서울 마포구 어울마당로 35',
+  genre: 'rockmetal',
+  date: '2021-06-26-20:00',
+  coverImage: '',
+};
+
+const SECOND_EXAMPLE_DATA = {
+  title: '라니아 공연',
+  artist: '라니아',
+  stage: 'FF',
+  address: '서울 마포구 어울마당로 37',
+  genre: '인디락',
+  date: '2021-07-20-20:00',
+  coverImage: '',
+};
 
 describe('ReadConcerts 에서는,', () => {
   beforeEach(() => {
@@ -17,28 +38,10 @@ describe('ReadConcerts 에서는,', () => {
   });
 
   it('컴포넌트를 렌더링하면 공연 목록을 읽는다.', () => {
-    const EXAMPLE_DATA = {
-      title: '마스토돈',
-      artist: '마스토돈',
-      stage: '롤링홀',
-      address: '서울 마포구 어울마당로 35',
-      genre: 'rockmetal',
-      date: '2021-06-26-20:00',
-      coverImage: '',
-    };
-
-    const SECOND_EXAMPLE_DATA = {
-      title: '라니아 공연',
-      artist: '라니아',
-      stage: 'FF',
-      address: '서울 마포구 어울마당로 37',
-      genre: '인디락',
-      date: '2021-07-20-20:00',
-      coverImage: '',
-    };
-
+    // given, when
     jest.spyOn(api, 'readConcertApi').mockResolvedValue([EXAMPLE_DATA, SECOND_EXAMPLE_DATA]);
 
+    // then
     waitFor(() => {
       const concerts = screen.getAllByRole('listitem');
       const firstConcert = concerts[0];
@@ -58,6 +61,32 @@ describe('ReadConcerts 에서는,', () => {
       expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.stage);
       expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.address);
       expect(secondConcert).toHaveTextContent(SECOND_EXAMPLE_DATA.date);
+    });
+  });
+
+  it('공연 이름으로 원하는 공연을 검색할 수 있다.', () => {
+    inputTextByLabel('title-search', EXAMPLE_DATA.title);
+
+    // then
+    waitFor(() => {
+      const concerts = screen.getAllByRole('listitem');
+      const firstConcert = concerts[0];
+
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.title);
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.artist);
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.genre);
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.stage);
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.address);
+      expect(firstConcert).toHaveTextContent(EXAMPLE_DATA.date);
+
+      const secondConcert = concerts[1];
+
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.title);
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.artist);
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.genre);
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.stage);
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.address);
+      expect(secondConcert).not.toHaveTextContent(SECOND_EXAMPLE_DATA.date);
     });
   });
 });
