ab826930c467b63fdfee23c7e2dde9a78b39bfb6
irrationnelle <drakkarverenis@gmail.com>
2021-07-31 15:23:34 +0900

    feat: Remove infowindow from kakao map api

--- src/ConcertLocationMap.tsx
+++ src/ConcertLocationMap.tsx
@@ -8,11 +8,12 @@ declare let window: CustomWindow;
 
 interface Props {
     address: string;
+    setPos?: ({ x, y }: {x: number, y: number}) => void;
 }
 
 const KAKAO_MAP_SDK_URL = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`;
 
-const ConcertLocationMap: React.FC<Props> = ({ address }: Props): ReactElement => {
+const ConcertLocationMap: React.FC<Props> = ({ address, setPos }: Props): ReactElement => {
   useEffect(() => {
     const script = document.createElement('script');
     script.async = true;
@@ -24,13 +25,6 @@ const ConcertLocationMap: React.FC<Props> = ({ address }: Props): ReactElement =
 
       kakao.maps.load(() => {
         const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
-        const options = {
-          // 지도를 생성할 때 필요한 기본 옵션
-          center: new kakao.maps.LatLng(37.54839846253257, 126.92007036208365), // 지도의 중심좌표.
-          level: 3, // 지도의 레벨(확대, 축소 정도)
-        };
-
-        const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
 
         const geocoder = new kakao.maps.services.Geocoder();
 
@@ -39,6 +33,18 @@ const ConcertLocationMap: React.FC<Props> = ({ address }: Props): ReactElement =
         geocoder.addressSearch(address, (result: any, status: any) => {
           // 정상적으로 검색이 완료됐으면
           if (status === kakao.maps.services.Status.OK) {
+            if (setPos) {
+              setPos({ x: result[0].x, y: result[0].y });
+            }
+
+            const options = {
+              // 지도를 생성할 때 필요한 기본 옵션
+              center: new kakao.maps.LatLng(result[0].y, result[0].x), // 지도의 중심좌표.
+              level: 3, // 지도의 레벨(확대, 축소 정도)
+            };
+
+            const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
             const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
             // 결과값으로 받은 위치를 마커로 표시합니다
@@ -47,25 +53,29 @@ const ConcertLocationMap: React.FC<Props> = ({ address }: Props): ReactElement =
               position: coords,
             });
 
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            const infowindow = new kakao.maps.InfoWindow({
-              content: '<div data-testid="kakaoMap" style="width:150px;text-align:center;padding:6px 0;">공연장</div>',
-            });
-            infowindow.open(map, marker);
+            // // 인포윈도우로 장소에 대한 설명을 표시합니다
+            // const infowindow = new kakao.maps.InfoWindow({
+            // eslint-disable-next-line max-len
+            //   content: '<div data-testid="kakaoMap" style="width:150px;text-align:center;padding:6px 0;">공연장</div>',
+            // });
+            // infowindow.open(map, marker);
 
             // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
             map.setCenter(coords);
+
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(map);
           }
         });
       });
     };
-  }, [address]);
+  }, [address, setPos]);
   return <MapWrapper id="map" />;
 };
 
 const MapWrapper = styled.div`
-    width: 100%;
-    height: 100%;
+    width: 250px;
+    height: 250px;
 `;
 
 export default ConcertLocationMap;
